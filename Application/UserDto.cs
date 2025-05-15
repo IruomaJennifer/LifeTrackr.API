29a99d2fd39a75fd6044c9de6c5ca1c21db467e9
IruomaJennifer <iruomaonyia@outlook.com>
2025-05-15 14:05:31 +0100

    add create-user functionaity

---
+++ Application/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LifeTrackr.API.Application
+{
+    public class UserDto
+    {
+        public string Email { get; set; } = default!;
+        public string FullName { get; set; } = default!;
+        
+    }
+}
