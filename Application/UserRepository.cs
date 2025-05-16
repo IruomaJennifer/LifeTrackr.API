10f944b7f0fef1f954e3a18eec1b506ef62bfed2
IruomaJennifer <iruomaonyia@outlook.com>
2025-05-16 01:18:12 +0100

    Revert "Merge pull request #1 from IruomaJennifer/feat/user-CRUD-operations"
    This reverts commit 11c3ca275576bf396169b07ce13313dfe7947dd7, reversing
    changes made to 67176477561c4651de479c1fae2d31df0bf251c9.

--- Application/UserRepository.cs
+++ Application/UserRepository.cs
@@ -2,30 +2,5 @@
 {
     public class UserRepository
     {
-        public static List<User> _users;
-
-        public UserRepository()
-        {
-            PopulateUsers();
-        }
-
-        public int CreateUser(UserDto newUser)
-        {
-            var userId = _users.OrderBy(u => u.Id).Last().Id++;
-            _users.Add(new User { Email = newUser.Email, FullName = newUser.FullName, CreatedAt = DateTime.UtcNow, Id = userId });
-            return userId;
-        }
-        
-        
-        private void PopulateUsers()
-        {
-
-            _users.Add(new User { Email = "peter.parker@gmail.com", FullName = "Peter Parker", Id = 1, CreatedAt = DateTime.UtcNow });
-            _users.Add(new User { Email = "jane.sanders@gmail.com", FullName = "Jane Sanders", Id = 2, CreatedAt = DateTime.UtcNow });
-            _users.Add(new User { Email = "john.mcvein@gmail.com", FullName = "John Mcvein", Id = 3, CreatedAt = DateTime.UtcNow });
-            _users.Add(new User { Email = "joel.vermont@gmail.com", FullName = "Joel Vermont", Id = 4, CreatedAt = DateTime.UtcNow });
-            _users.Add(new User { Email = "anthonia.rivera@gmail.com", FullName = "Anthonia Rivera", Id = 5, CreatedAt = DateTime.UtcNow });
-            
-        }
     }
 }
