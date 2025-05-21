669a22750f40ae5f519ffc26b03f8a65b7af0007
IruomaJennifer <iruomaonyia@outlook.com>
2025-05-21 09:48:30 +0100

    add get users endpoint

---
+++ Application/FetchUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LifeTrackr.API.Application;
+
+public class FetchUserDto
+{
+    public string Email { get; set; } = default!;
+    public string FullName { get; set; } = default!;
+    public int Id { get; set; }
+}
